7e743c58f601b7f242ae3871923cf10d02369e97
faebser <fabian.frei@esf-frei.ch>
2014-01-29 00:02:08 +0100

    big refactoring in main

--- backend/hakunaaaMatataRobotoWebsito.py
+++ backend/hakunaaaMatataRobotoWebsito.py
@@ -9,29 +9,6 @@ from classes.config import Config, ImageSize
 from classes.entry import Entry
 from classes.imageHandling import fromJsonToImage
 
-#logging
-log.basicConfig(format='%(levelname)s: %(message)s', level=log.DEBUG)
-
-# loading config
-config = Config()
-
-# paths
-currentPath = path.dirname(path.realpath(__file__))
-config.addPath("templatePath", path.join(currentPath, "templates"))
-config.addPath("partialsPath", path.join(currentPath, "templates", "partials"))
-config.addPath("contentPath", path.join(currentPath, "content"))
-config.addPath("website", path.join(currentPath, "website"))
-config.addPath("images", path.join(config.getPath("website"), 'img'))
-config.addPath("overview", path.join(config.getPath("contentPath"), 'overview'))
-config.addPath("pages", path.join(config.getPath("contentPath"), 'pages'))
-config.addPath("promoted", path.join(config.getPath("contentPath"), 'promoted_stuff'))
-
-log.debug("-- paths --")
-log.debug("currentPath: " + currentPath)
-log.debug("templatePath: " + config.getPath("templatePath"))
-log.debug("partialsPath: " + config.getPath("partialsPath"))
-log.debug("contentPath: " + config.getPath("contentPath"))
-
 
 # functions depending on global config object. Pretty ugly, I know
 def iterateOverPosterImages(projectName, currentProjectPath, entry, jsonFile):
@@ -44,7 +21,8 @@ def iterateOverOverviewImages(projectName, currentProjectPath, entry, jsonFile):
 
 def iterateOverImages(projectName, currentProjectPath, entry, nodeName, jsonFile):
     for element, value in jsonFile[nodeName].iteritems():
-        results = fromJsonToImage(element, value, config.getPath("images"), log, config, projectName, currentProjectPath)
+        results = fromJsonToImage(element, value, config.getPath("images"), log, config, projectName,
+                                  currentProjectPath)
         for imageSize, imagePath in results.iteritems():
             if 'overviewImage' in nodeName:
                 entry.addOverViewImage(imageSize, imagePath)
@@ -58,7 +36,8 @@ def iterateOverAllImages(projectName, currentProjectPath, entry, imageList):
     for imageObject in imageList:
         index = imageList.index(imageObject)
         for element, value in imageObject.iteritems():
-            results = fromJsonToImage(element, value, config.getPath("images"), log, config, projectName, currentProjectPath)
+            results = fromJsonToImage(element, value, config.getPath("images"), log, config, projectName,
+                                      currentProjectPath)
             if index is 0:
                 entryData = {}
             else:
@@ -67,91 +46,119 @@ def iterateOverAllImages(projectName, currentProjectPath, entry, imageList):
                 entryData[imageSize] = imagePath
             entry.addImage(index, entryData)
 
-# main starts here
-configJsonFile = json.loads(open(path.join(currentPath, "config.json")).read())
-config.addImageSizes(configJsonFile["imageSizes"])
-config.addRowTypes(configJsonFile["rowTypes"])
+def main():
+    #logging
+    log.basicConfig(format='%(levelname)s: %(message)s', level=log.DEBUG)
 
-# partials
-partials = {"entry-1": open(path.join(config.getPath("partialsPath"), "partial-entry-1.html")).read(),
-            "entry-2": open(path.join(config.getPath("partialsPath"), "partial-entry-2.html")).read(),
-            "entry-3": open(path.join(config.getPath("partialsPath"), "partial-entry-3.html")).read()}
+    # loading config
+    config = Config()
 
-# pystache renderer init
-renderer = pystache.Renderer(search_dirs=config.getPath("templatePath"), file_extension="html", partials=partials)
+    # paths
+    currentPath = path.dirname(path.realpath(__file__))
+    config.addPath("templatePath", path.join(currentPath, "templates"))
+    config.addPath("partialsPath", path.join(currentPath, "templates", "partials"))
+    config.addPath("contentPath", path.join(currentPath, "content"))
+    config.addPath("website", path.join(currentPath, "website"))
+    config.addPath("images", path.join(config.getPath("website"), 'img'))
+    config.addPath("overview", path.join(config.getPath("contentPath"), 'overview'))
+    config.addPath("pages", path.join(config.getPath("contentPath"), 'pages'))
+    config.addPath("promoted", path.join(config.getPath("contentPath"), 'promoted_stuff'))
 
-# load templates
-renderer.load_template("overviewRow")
-renderer.load_template("promoEntryAndPage")
-renderer.load_template("backgrounds")
-#renderer.load_template("skeleton")
+    log.debug("-- paths --")
+    log.debug("currentPath: " + currentPath)
+    log.debug("templatePath: " + config.getPath("templatePath"))
+    log.debug("partialsPath: " + config.getPath("partialsPath"))
+    log.debug("contentPath: " + config.getPath("contentPath"))
 
-#load promoted content
-promotedDirs = listdir(config.getPath('promoted'))
-promotedDirs.sort()
-log.debug("list of dirs: " + str(promotedDirs))
+    configJsonFile = json.loads(open(path.join(currentPath, "config.json")).read())
+    config.addImageSizes(configJsonFile["imageSizes"])
+    config.addRowTypes(configJsonFile["rowTypes"])
 
-htmlContent = ''
-cssContent = ''
+    # partials
+    partials = {"entry-1": open(path.join(config.getPath("partialsPath"), "partial-entry-1.html")).read(),
+                "entry-2": open(path.join(config.getPath("partialsPath"), "partial-entry-2.html")).read(),
+                "entry-3": open(path.join(config.getPath("partialsPath"), "partial-entry-3.html")).read()}
 
-for currentDir in promotedDirs:
-    if currentDir.startswith("."):
-        pass
-    else:
-        projectName = currentDir
-        currentProjectPath = path.join(config.getPath('promoted'), projectName)
-        try:
-            currentJsonFile = json.loads(open(path.join(currentProjectPath, 'data.json')).read())
-        except ValueError:
-            log.exception("Error while reading json-file: " + path.join(currentProjectPath, 'data.json'))
-            exit(1)
-        currentEntry = Entry()
-        currentEntry.simpleFillWithDict(currentJsonFile)
-        currentEntry.setId(currentJsonFile['title'])
-        iterateOverPosterImages(projectName, currentProjectPath, currentEntry, currentJsonFile)
-        iterateOverOverviewImages(projectName, currentProjectPath, currentEntry, currentJsonFile)
-        if isinstance(currentJsonFile['images'], (unicode, str)):
-            imageSizes = config.getAllImageSizes()
-            imageDict = []
-            for subdir, dirs, files in walk(path.join(currentProjectPath, currentJsonFile['images'])):
-                posterImages = [path.split(key)[1] for key in currentJsonFile['posterImage'].keys()]
-                overviewImages = [path.split(key)[1] for key in currentJsonFile['overviewImage'].keys()]
-                log.debug(posterImages)
-                log.debug(overviewImages)
-                for image in files:
-                    log.debug(image)
-                    if not image in posterImages and not image in overviewImages:
-                        imageDict.extend({image: imageSizes})
-                    else:
-                        log.debug("image already used: " + str(image))
-            iterateOverAllImages(projectName, currentProjectPath, currentEntry, imageDict)
+    # pystache renderer init
+    renderer = pystache.Renderer(search_dirs=config.getPath("templatePath"), file_extension="html", partials=partials)
+
+    # load templates
+    renderer.load_template("overviewRow")
+    renderer.load_template("promoEntryAndPage")
+    renderer.load_template("backgrounds")
+    #renderer.load_template("skeleton")
+
+    #load promoted content
+    promotedDirs = listdir(config.getPath('promoted'))
+    promotedDirs.sort()
+    log.debug("list of dirs: " + str(promotedDirs))
+
+    htmlContent = ''
+    cssContent = ''
+
+    for currentDir in promotedDirs:
+        if currentDir.startswith("."):
+            pass
         else:
-            iterateOverAllImages(projectName, currentProjectPath, currentEntry, currentJsonFile['images'])
-        #log.debug(currentEntry)
-        htmlContent += renderer.render_name('promoEntryAndPage', currentEntry)
-        cssContent += renderer.render_name('backgrounds', currentEntry)
-        #log.debug("css: " + cssContent)
-        log.debug("content: " + htmlContent)
+            projectName = currentDir
+            currentProjectPath = path.join(config.getPath('promoted'), projectName)
+            try:
+                currentJsonFile = json.loads(open(path.join(currentProjectPath, 'data.json')).read())
+            except ValueError:
+                log.exception("Error while reading json-file: " + path.join(currentProjectPath, 'data.json'))
+                exit(1)
+            currentEntry = Entry()
+            currentEntry.simpleFillWithDict(currentJsonFile)
+            currentEntry.setId(currentJsonFile['title'])
+            iterateOverPosterImages(projectName, currentProjectPath, currentEntry, currentJsonFile)
+            iterateOverOverviewImages(projectName, currentProjectPath, currentEntry, currentJsonFile)
+            if isinstance(currentJsonFile['images'], (unicode, str)):
+                imageSizes = config.getAllImageSizes()
+                imageDict = []
+                for subdir, dirs, files in walk(path.join(currentProjectPath, currentJsonFile['images'])):
+                    posterImages = [path.split(key)[1] for key in currentJsonFile['posterImage'].keys()]
+                    overviewImages = [path.split(key)[1] for key in currentJsonFile['overviewImage'].keys()]
+                    log.debug(posterImages)
+                    log.debug(overviewImages)
+                    for image in files:
+                        log.debug(image)
+                        if not image in posterImages and not image in overviewImages:
+                            imageDict.extend({image: imageSizes})
+                        else:
+                            log.debug("image already used: " + str(image))
+                iterateOverAllImages(projectName, currentProjectPath, currentEntry, imageDict)
+            else:
+                iterateOverAllImages(projectName, currentProjectPath, currentEntry, currentJsonFile['images'])
+                #log.debug(currentEntry)
+            htmlContent += renderer.render_name('promoEntryAndPage', currentEntry)
+            cssContent += renderer.render_name('backgrounds', currentEntry)
+            #log.debug("css: " + cssContent)
+            log.debug("content: " + htmlContent)
 
-#load overview content
+    #load overview content
 
-overviewJsonFile = json.loads(open(path.join(config.getPath('overview'), 'overview.json')).read())
+    overviewJsonFile = json.loads(open(path.join(config.getPath('overview'), 'overview.json')).read())
 
-for row in overviewJsonFile['rows']:
-    templateContent = {}
-    rowType = config.getRowTypeOrNone(row['type'])
-    if rowType is None:
-        log.error("Invalid Type with value: " + row['type'] + " in overview.json")
-        pass
-    else:
-        for index, entry in enumerate(row['entries']):
-            log.debug("entry and index: " + str(index) + " / " + str(entry))
-            overviewEntry = Entry()
-            overviewEntry.simpleFillWithDict(currentJsonFile)
-            overviewEntry.setId(currentJsonFile['title'])
-            iterateOverPosterImages(projectName, currentProjectPath, overviewEntry, currentJsonFile)
-            iterateOverOverviewImages(projectName, currentProjectPath, overviewEntry, currentJsonFile)
-            templateContent['entry-' + str(index)] = overviewEntry
+    for row in overviewJsonFile['rows']:
+        templateContent = {}
+        rowType = config.getRowTypeOrNone(row['type'])
+        if rowType is None:
+            log.error("Invalid Type with value: " + row['type'] + " in overview.json")
+            pass
+        else:
+            for index, entry in enumerate(row['entries']):
+                log.debug("entry and index: " + str(index) + " / " + str(entry))
+                overviewEntry = Entry()
+                overviewEntry.simpleFillWithDict(currentJsonFile)
+                overviewEntry.setId(currentJsonFile['title'])
+                iterateOverPosterImages(projectName, currentProjectPath, overviewEntry, currentJsonFile)
+                iterateOverOverviewImages(projectName, currentProjectPath, overviewEntry, currentJsonFile)
+                templateContent['entry-' + str(index)] = overviewEntry
+
+
+if __name__ == "__main__":
+    log.debug("main called")
+    main()
 
 #ouput = renderer
 
